07ddacdcb18942a6e1a0087ec95889e536139b19
umaedi <umaedi.kh.99@gmail.com>
2026-05-22 13:00:45 +0700

    ubah ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,13 +3,16 @@ module.exports = {
     {
       name: 'cloud-api-diskominfo',
       script: './dist/index.js',
-      instances: 'max',       // Run in cluster mode utilizing all CPU cores
-      exec_mode: 'cluster',   // Cluster mode for load balancing and high availability
-      watch: false,           // Do not watch files in production
-      max_memory_restart: '1G', // Restart app if memory exceeds 1GB
+      // instances: 'max',       // Run in cluster mode utilizing all CPU cores (Overkill)
+      // exec_mode: 'cluster',   // Cluster mode for load balancing (Overkill)
+      // max_memory_restart: '1G', // Restart app if memory exceeds 1GB (Overkill)
+      instances: 1,          // Hanya 1 instance saja
+      exec_mode: 'fork',     // Mode sederhana (fork), hemat penggunaan RAM & CPU
+      watch: false,
+      max_memory_restart: '200M', // Batas aman 200MB untuk service ringan
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 9932
       }
     }
   ]
